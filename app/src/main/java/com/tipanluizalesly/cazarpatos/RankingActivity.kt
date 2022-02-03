a2d8f2ad469def55e197f09ec003e5874ead5411
Lesly Tipanluiza <lesly.tipanluiza@epn.edu.ec>
2022-02-03 17:05:30 -0500

    Lab08 Firebase-Firestore finished

--- app/src/main/java/com/tipanluizalesly/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/tipanluizalesly/cazarpatos/RankingActivity.kt
@@ -2,8 +2,13 @@ package com.tipanluizalesly.cazarpatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.tipanluizalesly.cazarpatos.database.RankingPlayerDBHelper
 
 class RankingActivity : AppCompatActivity() {
@@ -14,6 +19,35 @@ class RankingActivity : AppCompatActivity() {
         OperacionesSqLite()
         GrabarRankingSQLite()
         LeerRankingsSQLite()
+
+
+        consultarPuntajeJugadores()
+
+    }
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("patosCazados", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Jugador>()
+                for (document in result) {
+                    val jugador = document.toObject(Jugador::class.java)
+                    //val jugador = document.toObject<Jugador>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
     fun OperacionesSqLite(){
         RankingPlayerDBHelper(this).deleteAllRanking()
